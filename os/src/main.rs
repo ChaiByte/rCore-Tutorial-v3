d7dba052b074454a8eafe4d90eab6f362fe7bdad
ChaiEnjoy <acdoge.cao@gmail.com>
2023-11-20 17:07:11 +0800

    get global symbol directly

--- os/src/main.rs
+++ os/src/main.rs
@@ -27,48 +27,62 @@ global_asm!(include_str!("entry.asm"));
 /// clear BSS segment
 pub fn clear_bss() {
     extern "C" {
-        fn sbss();
-        fn ebss();
+        static mut sbss: u64;
+        static mut ebss: u64;
     }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    unsafe {
+        let mut ptr = &mut sbss as *mut u64;
+        let end = &mut ebss as *mut u64;
+        while ptr < end {
+            ptr.write_volatile(0);
+            ptr = ptr.offset(1);
+        }
+    }
 }
 
 /// the rust entry-point of os
 #[no_mangle]
 pub fn rust_main() -> ! {
     extern "C" {
-        fn stext(); // begin addr of text segment
-        fn etext(); // end addr of text segment
-        fn srodata(); // start addr of Read-Only data segment
-        fn erodata(); // end addr of Read-Only data ssegment
-        fn sdata(); // start addr of data segment
-        fn edata(); // end addr of data segment
-        fn sbss(); // start addr of BSS segment
-        fn ebss(); // end addr of BSS segment
-        fn boot_stack_lower_bound(); // stack lower bound
-        fn boot_stack_top(); // stack top
+        static mut stext: u64; // begin addr of text segment
+        static mut etext: u64; // end addr of text segment
+        static mut srodata: u64; // start addr of Read-Only data segment
+        static mut erodata: u64; // end addr of Read-Only data ssegment
+        static mut sdata: u64; // start addr of data segment
+        static mut edata: u64; // end addr of data segment
+        static mut sbss: u64; // start addr of BSS segment
+        static mut ebss: u64; // end addr of BSS segment
+        static mut boot_stack_lower_bound: u64; // stack lower bound
+        static mut boot_stack_top: u64; // stack top
     }
     clear_bss();
     logging::init();
     println!("[kernel] Hello, world!");
     trace!(
         "[kernel] .text [{:#x}, {:#x})",
-        stext as usize,
-        etext as usize
+        unsafe { stext as usize },
+        unsafe { etext as usize }
     );
     debug!(
         "[kernel] .rodata [{:#x}, {:#x})",
-        srodata as usize, erodata as usize
+        unsafe { srodata as usize },
+        unsafe { erodata as usize }
     );
     info!(
         "[kernel] .data [{:#x}, {:#x})",
-        sdata as usize, edata as usize
+        unsafe { sdata as usize },
+        unsafe { edata as usize }
     );
     warn!(
         "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
-        boot_stack_top as usize, boot_stack_lower_bound as usize
+        unsafe { boot_stack_top as usize },
+        unsafe { boot_stack_lower_bound as usize }
     );
-    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    error!(
+        "[kernel] .bss [{:#x}, {:#x})",
+        unsafe { sbss as usize },
+        unsafe { ebss as usize }
+    );
 
     // CI autotest success: sbi::shutdown(false)
     // CI autotest failed : sbi::shutdown(true)
